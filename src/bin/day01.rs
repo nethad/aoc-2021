e6134790ba97e814a4bafa5ccd5fc0cbb8699eb0
Thomas Ritter <thomas.ritter@mocoapp.com>
2021-12-02 22:06:51 +0100

    refactor file structure

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 fn main() {
-    let file = File::open("src/bin/day01/input.txt").expect("Could not open file!");
+    let file = File::open("src/bin/inputs/day01.txt").expect("Could not open file!");
     let lines = BufReader::new(file)
         .lines()
         .filter_map(|line| line.unwrap().parse::<usize>().ok())
@@ -13,4 +13,17 @@ fn main() {
 
     println!("Q: How many measurements are larger than the previous measurement?");
     println!("A: {}", increase_count);
+
+    let group_sums = lines
+        .windows(3)
+        .map(|group| group[0] + group[1] + group[2])
+        .collect::<Vec<_>>();
+
+    let increase_count = group_sums
+        .windows(2)
+        .filter(|pair| pair[1] > pair[0])
+        .count();
+
+    println!("Q: How many sums are larger than the previous sum?");
+    println!("A: {}", increase_count)
 }
