3fd79dac1d0b4994f0b6b1c6ef5939047bb410ca
Thomas Ritter <thomas.ritter@mocoapp.com>
2021-12-02 22:17:40 +0100

    day 02: part 2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -13,7 +13,7 @@ fn main() {
     let mut horizontal_position = 0;
     let mut depth = 0;
 
-    for step in lines {
+    for step in &lines {
         let parts = step.split(" ").collect::<Vec<_>>();
         let direction = parts[0];
         let unit = parts[1].parse::<usize>().ok();
@@ -27,6 +27,31 @@ fn main() {
 
     let result = horizontal_position * depth;
 
+    println!(
+        "Q: What do you get if you multiply your final horizontal position by your final depth?"
+    );
+    println!("A: {}", result);
+
+    horizontal_position = 0;
+    depth = 0;
+    let mut aim = 0;
+
+    for step in &lines {
+        let parts = step.split(" ").collect::<Vec<_>>();
+        let direction = parts[0];
+        let unit = parts[1].parse::<usize>().ok();
+        match (direction, unit) {
+            ("forward", Some(unit)) => {
+                horizontal_position += unit;
+                depth += aim * unit;
+            }
+            ("up", Some(unit)) => aim -= unit,
+            ("down", Some(unit)) => aim += unit,
+            _ => println!("UNKNOWN!"),
+        }
+    }
+
+    let result = horizontal_position * depth;
     println!(
         "Q: What do you get if you multiply your final horizontal position by your final depth?"
     );
